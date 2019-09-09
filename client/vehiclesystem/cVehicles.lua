7cb0c7c084e8fc99c445049dd6bdfb9735c8025e
SkydoxTV <jannikfried40@gmail.com>
2019-09-09 17:21:13 +0200

    Führerscheinklassen erstellt (vorläufig) Admin Duty Sonderberechtigung hinzugefügt.
    Admin Duty wird später eingeführt.
    Momentan ist der Admin Duty nur per Admin Panel ('p') zu erhalten -> Muss manuell per setElementData(Element,"adminduty",true) gesetzt werden.

--- client/vehiclesystem/cVehicles.lua
+++ client/vehiclesystem/cVehicles.lua
@@ -91,3 +91,118 @@ addEventHandler("onClientClick",getRootElement(),function(btn,st,ax,ay,x,y,z,ce)
         end
     end
 end)
+
+local airplanes = {592,577,511,512,593,520,553,476,519,460,513}
+local helicopters = {548,425,417,487,488,497,563,447,469}
+local boats = {572,473,493,595,484,430,453,452,446,454}
+local bikes = {581,509,481,462,521,463,510,522,461,448,468,586}
+local buses = {431,437}
+local lkws = {407,544,433,427,499,609,498,524,532,578,573,455,403,423,414,443,515,514,456}
+
+addEventHandler("onClientVehicleStartEnter",getRootElement(),function(player,seat,door)
+    if not lp:getData("adminduty") then
+        if seat == 0 then
+            local airplane = false
+            local helicopter = false
+            local boat = false
+            local bike = false
+            local bus = false
+            local lkw = false
+            for k,v in pairs(airplanes) do
+                if not airplane then 
+                    if v == source:getModel() then
+                        airplane = true
+
+                    else
+                        airplane = false
+                    end
+                end
+            end
+            for k,v in pairs(helicopters) do
+                if not helicopter then
+                    if v == source:getModel() then
+                        helicopter = true
+                    else
+                        helicopter = false
+                    end
+                end
+            end
+            for k,v in pairs(boats) do
+                if not boat then
+                    if v == source:getModel() then
+                        boat = true
+                        
+                    else
+                        boat = false
+                    end
+                end
+            end
+            for k,v in pairs(bikes) do
+                if not bike then 
+                    if v == source:getModel() then
+                        bike = true
+                    else
+                        bike = false
+                    end
+                end
+            end
+            for k,v in pairs(buses) do
+                if not bus then
+                    if v == source:getModel() then
+                        bus = true
+                    else
+                        bus = false
+                    end
+                end
+            end
+            for k,v in pairs(lkws) do
+                if not lkw then
+                    if v == source:getModel() then
+                        lkw = true
+                    else
+                        lkw = false
+                    end
+                end
+            end
+
+
+            if (not airplane and not helicopter and not boat and not bike and not bus and not lkw) then
+                if lp:getData("b") == 0 then
+                    cancelEvent()
+                    notification("Du hast noch keinen Führerschein der Klasse 'B'",5000,tocolor(182,50,50,255))
+                end
+            elseif (bus) then
+                if lp:getData("d") == 0 then
+                    cancelEvent()
+                    notification("Du hast noch keinen Führerschein der Klasse 'D'",5000,tocolor(182,50,50,255))
+                end
+            elseif (boat) then
+                if lp:getData("e") == 0 then
+                    cancelEvent()
+                    notification("Du hast noch keinen Führerschein der Klasse 'E'",5000,tocolor(182,50,50,255))
+                end
+            elseif (lkw) then
+                if lp:getData("c") == 0 then
+                    cancelEvent()
+                    notification("Du hast noch keinen Führerschein der Klasse 'C'",5000,tocolor(182,50,50,255))
+                end
+            elseif (bike) then
+                if lp:getData("a") == 0 then
+                    cancelEvent()
+                    notification("Du hast noch keinen Führerschein der Klasse 'A'",5000,tocolor(182,50,50,255))
+                end
+            elseif (airplane) then
+                if lp:getData("ap") == 0 then
+                    cancelEvent()
+                    notification("Du hast noch keinen Führerschein der Klasse 'AP'",5000,tocolor(182,50,50,255))
+                end
+            elseif (helicopter) then
+                print(111)
+                if lp:getData("hc") == 0 then
+                    cancelEvent()
+                    notification("Du hast noch keinen Führerschein der Klasse 'HC'",5000,tocolor(182,50,50,255))
+                end
+            end
+        end
+    end
+end)
